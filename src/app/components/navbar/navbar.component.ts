5ce811666e5519c05b9f1c189efb48a342bfef85
Abderrahmane Zouakh <zzmoubarek@gmail.com>
2023-02-01 14:11:38 +0100

    Tasks nav routes to app

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -28,7 +28,7 @@ export class NavbarComponent implements OnInit {
 
   ngOnInit(): void {
 
-    if (this.authService.isLoggedIn()) {
+    if (this.isLoggedIn()) {
       this.navlinkService.getNavlinks().subscribe((links) => (this.navlinks = links));
       this.getUserName();
     }else{
