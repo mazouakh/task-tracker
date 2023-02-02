3db6f6e485f367850d3973f67f20c651c41b548f
Abderrahmane Zouakh <zzmoubarek@gmail.com>
2023-02-02 14:01:02 +0100

    update to angular 15

--- src/app/components/auth-forms/login-form/login-form.component.ts
+++ src/app/components/auth-forms/login-form/login-form.component.ts
@@ -5,52 +5,51 @@ import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.css']
+  styleUrls: ['./login-form.component.css'],
 })
 export class LoginFormComponent implements OnInit {
-
   // Attributes
-  @Output() displaySignupForm : EventEmitter<boolean> = new EventEmitter<boolean>();
+  @Output() displaySignupForm: EventEmitter<boolean> =
+    new EventEmitter<boolean>();
 
   // Form Values
   email!: string;
   password!: string;
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
     this.authService.clearLocalStorage();
   }
 
-  onSubmit(){
+  onSubmit() {
     this.login();
   }
 
-  
   isPasswordInCorrect: boolean = false;
   isUserUnfound: boolean = false;
 
-  login(){
+  login() {
     this.authService.getUserByEmail(this.email).subscribe((userFound) => {
-      
+      console.log('logging in...');
       if (userFound.length == 0) {
         this.isUserUnfound = true;
         this.isPasswordInCorrect = false;
         return;
       }
-      
+
       this.isUserUnfound = false;
       if (userFound[0].password === this.password) {
         this.isPasswordInCorrect = false;
         // Login in
-        this.authService.login(userFound[0])
-      }else{
+        this.authService.login(userFound[0]);
+      } else {
         this.isPasswordInCorrect = true;
       }
     });
   }
-  
-  showSigninForm(){
+
+  showSigninForm() {
     this.displaySignupForm.emit(true);
   }
 }
